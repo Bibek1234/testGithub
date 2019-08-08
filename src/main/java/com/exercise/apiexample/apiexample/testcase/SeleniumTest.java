6dbe03b1effb7d1b10176fb4702a1153ceae5e95
bibekbhurtel <bibek.bhurtel1995@gmail.com>
2019-08-07 21:23:16 -0600

    Added selenium test class

---
+++ src/main/java/com/exercise/apiexample/apiexample/testcase/SeleniumTest.java
@@ -0,0 +1,8 @@
+package com.exercise.apiexample.apiexample.testcase;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello this is test class");
+    }
+}
