c7ec4caef894d2215350bd58b5907458c553e49d
Bibek1234 <bibek.bhurtel1995@gmail.com>
2020-04-16 17:25:24 -0400

    test

--- src/main/java/com/exercise/apiexample/apiexample/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.apiexample.apiexample;
-
-public class Hello {
-
-
-    public void hello(){
-        System.out.println("Hello this is hello method in the Hello class");
-    }
-}
